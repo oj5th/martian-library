e7b7aacd8c28118e534881491062d8289ea22e97
OJ De Quintos <odequintos@yondu.com>
2021-11-17 10:43:13 +0800

    Adding tokens to Apollo client

--- app/javascript/utils/apollo.js
+++ app/javascript/utils/apollo.js
@@ -20,10 +20,21 @@ export const createCache = () => {
 const getToken = () =>
   document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 const token = getToken();
+
+const getTokens = () => {
+  const tokens = {
+    "X-CSRF-Token": document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute("content")
+  }
+  const authToken = localStorage.getItem("mlToken")
+  return authToken ? { ...tokens, Authorization: authToken } : tokens;
+}
+
 const setTokenForOperation = async operation =>
   operation.setContext({
     headers: {
-      'X-CSRF-TOKEN': token,
+      ...getTokens()
     },
   })
 // link with token
