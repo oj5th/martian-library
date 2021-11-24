e40f8fbef48259ee33c9b8a5ce519e7e1e2b1f90
OJ De Quintos <odequintos@yondu.com>
2021-11-24 10:07:40 +0800

    All you need is DRY

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -9,5 +9,11 @@
 
 module Mutations
   class BaseMutation < GraphQL::Schema::Mutation
+    def check_authentication!
+      return if context[:current_user]
+
+      raise GraphQL::ExecutionError,
+        "You need to authenticate to perform this action."
+    end
   end
 end
